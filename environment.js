67976acba002fba9b91cfd3a62787f9ff6d7bb65
Bogomip <alex.bunting@gmail.com>
2022-05-31 12:11:50 +0300

    login and basics

---
+++ environment.js
@@ -0,0 +1,8 @@
+const mysqlConnectionData = {
+    host: 'sql134.main-hosting.eu', 
+    user: 'u656701484_rednovaadmin',
+    password: 'r3dN0V4**!',
+    database: 'u656701484_rednova'
+};
+
+module.exports = mysqlConnectionData;
