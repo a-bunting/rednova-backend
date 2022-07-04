5b55a81b276c8bf21db0fb887a69030aa540fdbf
Bogomip <alex.bunting@gmail.com>
2022-07-04 15:26:37 +0300

    buy/sell complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Game = require("./game/game");
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
@@ -6,9 +7,10 @@ const userRoutes = require('./routes/user');
 const moveRoutes = require('./routes/move');
 const galaxyRoutes = require('./routes/galaxy');
 const planetRoutes = require('./routes/planet');
+const router = require('./routes/admin');
 
 const app = express();
-
+const game = new Game();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +22,16 @@ app.use((req, res, next) => {
   next();
 })
 
+
+// all the various api calls...
+// admin
 app.use("/api/administration", adminRoutes);
+
+// add to the game to any api call so websockets may be used in the routes except for admin
+// to shield admin changes from websockets.
+app.use((req, res, next) => { req['game'] = game; next(); })
+
+// general game api calls
 app.use("/api/user", userRoutes);
 app.use("/api/move", moveRoutes);
 app.use("/api/galaxy", galaxyRoutes);
--- routes/planet.js
+++ routes/planet.js
@@ -9,6 +9,7 @@ const pricing = require('../methods/pricing');
 const ship = require('../methods/ship-functions');
 
 router.get('/getPlanetData', checkAuth, (req, res, next) => {
+
     const userData = methods.getUserDataFromToken(req);
     const galaxyId = req.query.galaxyId;
     const planetId = req.query.planetId;
@@ -55,7 +56,6 @@ router.get('/getPlanetData', checkAuth, (req, res, next) => {
     })
 })
 
-// prime one for cleaning...
 router.post('/buyResources', checkAuth, (req, res, next) => {
     const userData = methods.getUserDataFromToken(req);
     const galaxyId = req.body.galaxyId;
@@ -87,7 +87,7 @@ router.post('/buyResources', checkAuth, (req, res, next) => {
             
             // check if this can be done, and if not, quit with response.!
             if(result[0][0].sector !== result[1][0].sectorid)  { 
-                res.status(400).json({ error: true, message: 'You are not in the same sector as that planet.', data: {}}); 
+                res.status(200).json({ error: true, message: 'You are not in the same sector as that planet.', data: {}}); 
             } else {
                
                 // if we dont have the storage capacity, buy the maximum amount...
@@ -102,28 +102,98 @@ router.post('/buyResources', checkAuth, (req, res, next) => {
                     totalCost = quantityToBuy * price;
                 }
 
-                // if the good doesnt exist on the ship it needsto be created, otherwise it needs to be updated.
-                let indexInShipJson = shipContents.findIndex(a => a.id === goodsToBuy.id);
-                const quantityToSet = indexInShipJson === -1 ? `JSON_OBJECT( 'id', '${goodsToBuy.id}', 'quantity', ${quantityToBuy} )` : shipContents[indexInShipJson].quantity + quantityToBuy;
-                const fieldToSet = indexInShipJson === -1 ? '' : '.quantity';
-                
-                indexInShipJson = indexInShipJson === -1 ? shipContents.length : indexInShipJson;
-    
-                // it can go ahead!
-                const updateQuery = `
-                    UPDATE universe__planets SET onPlanet = JSON_SET(onPlanet, '$[${goodOfTypeOnPlanet}].current', ${planetaryGoods[goodOfTypeOnPlanet].current - quantityToBuy}) WHERE id=${planetId} ; 
-                    UPDATE ships__users SET money = money - ${totalCost}, storage = JSON_SET(storage, '$[${indexInShipJson}]${fieldToSet}', ${quantityToSet}) WHERE galaxyid=${galaxyId} AND userid=${userData.id}
-                `;
+                if(quantityToBuy === 0) {
+                    res.status(200).json({ error: true, message: `You have no more space on your ship!`, data: {}});
+                } else {
+                    // if the good doesnt exist on the ship it needsto be created, otherwise it needs to be updated.
+                    let indexInShipJson = shipContents.findIndex(a => a.id === goodsToBuy.id);
+                    const quantityToSet = indexInShipJson === -1 ? `JSON_OBJECT( 'id', '${goodsToBuy.id}', 'name', '${pricing.getNameFromId(goodsToBuy.id)}', 'quantity', ${quantityToBuy} )` : shipContents[indexInShipJson].quantity + quantityToBuy;
+                    const fieldToSet = indexInShipJson === -1 ? '' : '.quantity';
                     
-                // NEED TO CREATE A JSON OBJETC IN THE SHIPS THING AT START
-                // NEED TO UPDATE THE MOENY ON THE SHIP AND THE PLANET
-                
-                connection.query(updateQuery, (e, updated) => {
-                    connection.destroy(); // for now
+                    indexInShipJson = indexInShipJson === -1 ? shipContents.length : indexInShipJson;
+        
+                    // it can go ahead!
+                    const updateQuery = `
+                        UPDATE universe__planets SET onPlanet = JSON_SET(onPlanet, '$[${goodOfTypeOnPlanet}].current', ${planetaryGoods[goodOfTypeOnPlanet].current - quantityToBuy}) WHERE id=${planetId} ; 
+                        UPDATE ships__users SET money = money - ${totalCost}, storage = JSON_SET(storage, '$[${indexInShipJson}]${fieldToSet}', ${quantityToSet}) WHERE galaxyid=${galaxyId} AND userid=${userData.id}
+                    `;
+                                            
+                    connection.query(updateQuery, (e, updated) => {
+                        connection.destroy(); // for now
+        
+                        if(!e) res.status(200).json({ error: false, message: '', data: { quantity: quantityToBuy, total: totalCost}});
+                        else res.status(400).json({ error: true, message: `Update did not work: ${updateQuery}:${e}`, data: {}});
+                    })
+                }
+
+            }
+        })
+    })
+})
+
+router.post('/sellResources', checkAuth, (req, res, next) => {
+    const userData = methods.getUserDataFromToken(req);
+    const galaxyId = req.body.galaxyId;
+    const planetId = req.body.planetId;
+    const goodsToSell = req.body.goods;
+    const connection = mysql.createConnection({...db, multipleStatements: true});
+
+    // get your ship from the db for the necessary currency and space stuff.
+    const sql = `SELECT storage, sector FROM ships__users WHERE userid=${userData.id} AND galaxyid=${galaxyId} ; 
+                 SELECT population, onPlanet, sectorid FROM universe__planets WHERE galaxyid=${galaxyId} AND id=${planetId}`;
+
+    connection.connect(connectionError => {
+        connection.query(sql, (e, result) => {
+            if(e) console.log(`Error: ${e}`);
+
+            // check if this can be done, and if not, quit with response.
+            if(result[0][0].sector !== result[1][0].sectorid)  { 
+                connection.destroy();
+                res.status(200).json({ error: true, message: 'You are not in the same sector as that planet.', data: {}}); 
+            } else {
+                const planetaryGoods = JSON.parse(result[1][0].onPlanet);
+                const shipContents = JSON.parse(result[0][0].storage);
     
-                    if(!e) res.status(200).json({ error: false, message: '', data: {}});
-                    else res.status(400).json({ error: true, message: `Update did not work: ${updateQuery}:${e}`, data: {}});
-                })
+                // get the id of the good in the storage arrays
+                const goodOfTypeOnShip = shipContents.findIndex(a => a.id === goodsToSell.id);
+                const goodOfTypeOnPlanet = planetaryGoods.findIndex(a => a.id === goodsToSell.id);
+
+                // selling goods so find how many are available
+                const goodsAvailable = shipContents[goodOfTypeOnShip].quantity;
+    
+                // price based upon planetary conditions, not ship conditions
+                const price = pricing.getPrice(goodsToSell.id, planetaryGoods[goodOfTypeOnPlanet].current, result[1][0].population).sell;
+    
+                // if there arent enough goods available, sell the max!
+                let quantityToSell = goodsToSell.quantity < goodsAvailable ? goodsToSell.quantity : goodsAvailable;
+                let totalCost = quantityToSell * price;
+
+                if(quantityToSell > 0) {
+    
+                    // if the good doesnt exist on the planet it needs to be created, otherwise it needs to be updated.
+                    // unlikey right now... but do this to allow for rare items not added at creation.
+                    let indexOnPlanet = planetaryGoods.findIndex(a => a.id === goodsToSell.id);
+                    const quantityToSet = indexOnPlanet === -1 ? `JSON_OBJECT( 'id', '${goodsToSell.id}', 'name', '${pricing.getNameFromId(goodsToSell.id)}', 'quantity', ${quantityToSell} )` : planetaryGoods[indexOnPlanet].current + quantityToSell;
+                    const fieldToSet = indexOnPlanet === -1 ? '' : '.current';
+                    
+                    indexOnPlanet = indexOnPlanet === -1 ? planetaryGoods.length : indexOnPlanet;
+        
+                    // it can go ahead!
+                    const updateQuery = `
+                        UPDATE universe__planets SET onPlanet = JSON_SET(onPlanet, '$[${goodOfTypeOnPlanet}]${fieldToSet}', ${quantityToSet}) WHERE id=${planetId} ; 
+                        UPDATE ships__users SET money = money + ${totalCost}, storage = JSON_SET(storage, '$[${goodOfTypeOnShip}].quantity', ${shipContents[goodOfTypeOnShip].quantity - quantityToSell}) WHERE galaxyid=${galaxyId} AND userid=${userData.id}
+                    `;
+                                            
+                    connection.query(updateQuery, (e, updated) => {
+                        connection.destroy(); // for now
+        
+                        if(!e) res.status(200).json({ error: false, message: '', data: { quantity: quantityToSell, total: totalCost}});
+                        else res.status(400).json({ error: true, message: `Update did not work: ${updateQuery}:${e}`, data: {}});
+                    })
+                } else {
+                    connection.destroy();
+                    res.status(200).json({ error: true, message: `You have no goods of that type to sell`, data: {}});
+                }
             }
         })
     })
