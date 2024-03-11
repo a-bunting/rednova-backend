81e1975334d1ca6edfa79581759f41205fb97a4b
Bogomip <alex.bunting@gmail.com>
2024-03-11 08:49:43 +0300

    upd

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const Game = require("./game/game");
 const bodyParser = require('body-parser');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    next();
+  });
+
 
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
@@ -10,18 +19,10 @@ const planetRoutes = require('./routes/planet');
 const tradeRoutes = require('./routes/trade');
 const router = require('./routes/admin');
 
-const app = express();
 const game = new Game();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-})
 
 // all the various api calls...
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: `${__dirname}/common.env`});
+// require('dotenv').config({ path: `${__dirname}/common.env`});
 const app = require("./app");
 const http = require("http");
 
